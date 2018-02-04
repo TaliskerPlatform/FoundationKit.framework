40453b09302d65401c80bcf61580c09f8f623ca1
Mo McRoberts <mo@nevali.net>
2018-02-04 23:40:18 +0000

    Move Travis `before_install` into a separate script.

---
+++ Resources/Travis/before_install.sh
@@ -0,0 +1,15 @@
+#! /bin/sh
+
+set -e
+set -x
+
+sudo apt-get install -qq build-essential binutils automake autoconf libtool pkg-config
+
+git clone --depth=50 --branch=develop https://github.com/TaliskerPlatform/idl.git
+cd idl
+git submodule update --init --recursive
+autoreconf -fvi
+./configure
+make
+sudo make install
+cd ..
