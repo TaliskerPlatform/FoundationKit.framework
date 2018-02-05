792b1f5fa2eb66054f5b9f72f3d7b03dde671940
Mo McRoberts <mo@nevali.net>
2018-02-05 04:13:00 +0000

    Tidy up FoundationKit/Declarations.h

--- Headers/Declarations.h
+++ Headers/Declarations.h
@@ -25,53 +25,69 @@
 # endif
 
 # if defined(__cplusplus)
-#  define DECLARE_INTERFACE(name) \
-	struct name
-#  define DECLARE_INTERFACE_(name, parent) \
-	struct name: virtual public parent
-#  define BEGIN_INTERFACE
-#  define END_INTERFACE
+
+/* COM interface declarations for C++ */
+
+#  define DECLARE_INTERFACE(name__) \
+	struct name__
+#  define DECLARE_INTERFACE_(name__, parent__) \
+	struct name__: virtual public parent__
+#  define BEGIN_INTERFACE /* */
+#  define END_INTERFACE /* */
 #  define THIS /* */
 #  define THIS_ /* */
 #  define PURE = 0
-#  define STDMETHOD_(type, name) \
-	virtual type __stdcall name
-#  define STDMETHOD(name) \
-	STDMETHOD(int, name)
+#  define STDMETHOD_(type__, name__) \
+	virtual type__ __stdcall name__
+#  define STDMETHOD(name__) \
+	STDMETHOD(int, name__)
+
 # else /*__cplusplus*/
-#  define DECLARE_INTERFACE(name) \
-	struct name { \
-		struct name##_vtable_ *lpVtbl; \
+
+/* COM interface declarations for C */
+		
+#  define DECLARE_INTERFACE(name__) \
+	struct name__ { \
+		struct name__##_vtable_ *lpVtbl; \
 	}; \
-	struct name##_vtable_
-#  define DECLARE_INTERFACE_(name, parent) \
+	struct name__##_vtable_
+#  define DECLARE_INTERFACE_(name__, parent__) \
 	DECLARE_INTERFACE(name)
-#  define BEGIN_INTERFACE
-#  define END_INTERFACE
-#  define THIS /* */ \
+#  define BEGIN_INTERFACE /* */
+#  define END_INTERFACE /* */
+#  define THIS \
 	struct INTERFACE *self
-#  define THIS_ /* */ \
+#  define THIS_ \
 	struct INTERFACE *self,
 #  define PURE /* */
-#  define STDMETHOD_(type, name) \
-	type __stdcall (*name)
-#  define STDMETHOD(name) \
-	STDMETHOD(int, name)
+#  define STDMETHOD_(type__, name__) \
+	type__ __stdcall (*name__)
+#  define STDMETHOD(name__) \
+	STDMETHOD(int, name__)
+
 # endif /*__cplusplus*/
 
 # undef TALISKER_EXPORT_
 # if defined(_WIN32) || defined(__CYGWIN__)
+
+/* On Windows, use dllimport/dllexport */
+		
 #  ifdef TALISKER_INTERNAL_
 #   define TALISKER_EXPORT_             __declspec(dllexport)
 #  else
 #   define TALISKER_EXPORT_             __declspec(dllimport)
 #  endif
+
 # else /*_WIN32 || __CYGWIN*/
+
+/* On other platforms, use __attribute__((visibility...)) if available */
+
 #  if defined(TALISKER_INTERNAL_) && __GNUC__ >= 4
 #   define TALISKER_EXPORT_             __attribute__((visibility("default")))
 #  else
 #   define TALISKER_EXPORT_
 #  endif
+
 # endif
 
 #endif /*!TALKISKER_DECLARATIONS_H_*/
